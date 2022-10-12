d99efc27cd7642fc684164681fa09a3b2cc5fccb
Rubankumar <115523439+Ruban6215@users.noreply.github.com>
2022-10-12 06:18:22 +0530

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,23 @@
+#Configs
+##############################################
+
+
+AUTOMOUNT=true
+
+PROPFILE=false
+
+POSTFSDATA=false
+
+LATESTARTSERVICE=false
+
+#IM
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "     Remove Phoenix Pill Notch  "
+  ui_print "*******************************"
+}
+
+REPLACE="
+/system/vendor/overlay
+"
